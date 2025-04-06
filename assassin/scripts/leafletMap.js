bae9c0df49cfd9b12720a6a7b5064b8a58b363a8
Kai <k.davey4@icloud.com>
2025-04-06 16:33:32 -0700

    zone

--- assassin/scripts/leafletMap.js
+++ assassin/scripts/leafletMap.js
@@ -7,25 +7,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Define the multi-section boundary (matching the image)
-var boundary3 = [
+var boundary = [
     [47.67803, -122.3157], // Top-left
     [47.67803, -122.3120], // Top-right
-    [47.6774, -122.3120], // Bottom-right
-    [47.6774, -122.3157], // Bottom-left 8
+    [47.6766, -122.3120], // Bottom-right
+    [47.6766, -122.3157], // Bottom-left 8
     [47.67803, -122.3157]  // Close the polygon
 ];
 
-var boundary4 = [
-    [47.6774, -122.3133], // Top-left y
-    [47.6774, -122.3120], // Top-right
-    [47.6766, -122.3120], // Bottom-right
-    [47.6766, -122.3133], // Bottom-left y
-    [47.6774, -122.3133]  // Close the polygon
-];
-
 // Add the boundary polygons
-L.polygon(boundary3, { color: 'green', fillOpacity: 0.3 }).addTo(map);
-L.polygon(boundary4, { color: 'green', fillOpacity: 0.3 }).addTo(map);
+L.polygon(boundary, { color: 'green', fillOpacity: 0.3 }).addTo(map);
 
 // Add a marker for Roosevelt High School
 L.marker([47.6776, -122.3127]).addTo(map)
