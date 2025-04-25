fa1fd9e78b401009966228030ed8b5cfabc2431d
Kai <k.davey4@icloud.com>
2025-04-25 15:12:00 -0700

    countdown and players

--- assassin/scripts/countdown.js
+++ assassin/scripts/countdown.js
@@ -1,5 +1,5 @@
 // Get the target date
-const targetDate = new Date("April 11, 2025 23:59:00").getTime();
+const targetDate = new Date("April 26, 2025 23:59:00").getTime();
 
 // Refresh how much time left every 1 second
 const countdownInterval = setInterval(function() {
@@ -13,10 +13,10 @@ const countdownInterval = setInterval(function() {
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
     document.getElementById("countdown").innerHTML =
-        "Round 2 Ends in: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        "Round 3 Ends in: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     if (timeLeft <= 0) {
         clearInterval(countdownInterval);
-        document.getElementById("countdown").innerHTML = "Round 2 is over!";
+        document.getElementById("countdown").innerHTML = "Round 3 is over!";
     }
 }, 1000);
